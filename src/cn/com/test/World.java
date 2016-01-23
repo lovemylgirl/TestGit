b1d171195a6d53501363c08668d4ebef9fa40f3e
murphys <murphys.tian@gmail.com>
2016-01-24 00:00:39 +0800

    add age

--- src/cn/com/test/World.java
+++ src/cn/com/test/World.java
@@ -5,6 +5,8 @@ public class World {
 	public static void main(String[] args) {
 		System.out.println("world !");
 		System.out.println("hello world !");
+		int age = 10;
+		System.out.println(age);
 	}
 
 }
